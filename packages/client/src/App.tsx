2e416ac70cebe4041f9de2d5c4b624080fd1d0f7
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-07-24 08:59:28 -0300

    feat(*): add trpc on client and change app file

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc } from "./trpc";
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: client</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
-ReactDOM.render(<App />, document.getElementById("app"));
+const queryClient = new QueryClient()
+
+const AppContent = () => {
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Name: client</div>
+      <div>Framework: react</div>
+      <div>Language: TypeScript</div>
+      <div>CSS: Tailwind</div>
+    </div>
+  );
+};
+
+const App = () => {
+  const [trpcClient] = useState(() => 
+    trpc.createClient({
+      url: "http://localhost:8080/trpc"
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <AppContent />
+      </QueryClientProvider>
+    </trpc.Provider>
+  )
+}
+ReactDOM.render(<AppContent />, document.getElementById("app"));
