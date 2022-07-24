b41647863b9ca3e73b95adad0ce64b3be051df27
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-07-24 10:49:17 -0300

    add inputs to client and state

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,18 +4,54 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { trpc } from "./trpc";
 
 import "./index.scss";
+const client = new QueryClient();
 
 const AppContent = () => {
   const getMessages = trpc.useQuery(['getMessages'])
 
+  const [user, setUser ] = useState("")
+  const [message, setMessage] = useState("")
+
+  const addMessage = trpc.useMutation("addMessage")
+  const onAdd = () => {
+    addMessage.mutate({
+      message,
+      user
+    },
+    {
+      onSuccess: () => {
+        client.invalidateQueries(["getMessages"])
+      }
+    });
+  }
   return (
     <div className="mt-10 text-3xl mx-auto max-w-6xl">
-      <div>{ JSON.stringify(getMessages.data) }</div>
+      <div>{(getMessages.data ?? []).map((row,index) => (
+        <div key={index}>
+          { JSON.stringify(row.message) }
+        </div>
+      ))}</div>
+
+      <input 
+        type="text" 
+        value={user} 
+        onChange={(e) => setUser(e.target.value)}
+        className="p-5 border-2 border-gray-300 rounded-lg w-full"
+        placeholder="User"
+      />
+      <input 
+        type="text" 
+        value={message} 
+        onChange={(e) => setMessage(e.target.value)}
+        className="p-5 border-2 border-gray-300 rounded-lg w-full"
+        placeholder="Messages"
+      />
+      <button onClick={onAdd}>Add Message</button>
     </div>
   );
 };
 
-const client = new QueryClient();
+
 const App = () => {
   const [trpcClient] = useState(() =>
     trpc.createClient({
