039b1f18c62e67584eaceaad5887ef94c67ed9c9
Mateus Sobrinho <mateus.webphp@gmail.com>
2022-07-24 10:34:01 -0300

    using zod and add mutation to server

--- packages/api-server/index.ts
+++ packages/api-server/index.ts
@@ -1,21 +1,55 @@
 import express from "express";
 import * as trpc from "@trpc/server"
 import * as trpcExpress from "@trpc/server/adapters/express"
+import cors from "cors"
+import { z } from "zod";
 
-const appRouter = trpc.router()
+interface ChatMessage {
+  user: string,
+  message: string
+}
+
+const messages: ChatMessage[] = [
+  { user: "user1", message: "hello" },
+  { user: "user2", message: "hi" },
+]
+
+const appRouter = trpc
+  .router()
   .query("hello", {
     resolve() {
-      return "Hello World";
+      return "Hello World II";
+    }
+  })
+  .query("getMessages", {
+    input: z.number().default(10),
+    resolve({ input }) {
+      return messages.slice(-input)
+    }
+  })
+  .mutation("addMessage", {
+    input: z.object({
+      user: z.string(),
+      message: z.string()
+    }),
+    resolve ({ input }) {
+      messages.push(input);
+      return input
     }
   })
 
+export type AppRouter = typeof appRouter;
+
 const app = express();
+app.use(cors())
 const port = 8080;
 
-app.use("/trpc", trpcExpress.createExpressMiddleware({
-  router: appRouter,
-  createContext: () => null,
-}))
+app.use("/trpc", 
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext: () => null,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello from api-server");
